7ef1b9e212f44e351c3a6198f447707e8c2196e2
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-03-21 15:33:52 +0530

    Adding the final code of the project

--- 01-Guess-A-Number/script.js
+++ 01-Guess-A-Number/script.js
@@ -4,18 +4,24 @@ let guessNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(guessNumber);
 let Score = 20;
 let higherscore = 0;
+
+//Refactoring
+
+function displayMessage(message) {
+  return (document.querySelector('.message').textContent = message);
+}
+
 document.querySelector('.check').addEventListener('click', function () {
   // acess the input value
   let inputNumber = Number(document.querySelector('.guess').value);
   console.log(inputNumber);
   //First condition if User didnot enter any Think
   if (!inputNumber) {
-    document.querySelector('.message').textContent = '🙏Please enter the Value';
+    displayMessage('🙏Please enter the Value');
   }
   //Second check is the guess number is equal to the input number
   else if (guessNumber === inputNumber) {
-    document.querySelector('.message').textContent =
-      '🏆 You won the match Buddy';
+    displayMessage('🏆 You won the match Buddy');
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').textContent = guessNumber;
     document.querySelector('.number').style.width = '30rem';
@@ -27,13 +33,16 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guessNumber !== inputNumber) {
     if (Score > 1) {
       inputNumber > guessNumber
-        ? (document.querySelector('.message').textContent = '📈 Too High')
-        : (document.querySelector('.message').textContent = '📈 Too Low');
+        ? displayMessage(
+            (document.querySelector('.message').textContent = '📈 Too High')
+          )
+        : displayMessage(
+            (document.querySelector('.message').textContent = '📈 Too Low')
+          );
       Score--;
       document.querySelector('.score').textContent = Score;
     } else {
-      document.querySelector('.message').textContent =
-        '🫂OOPS you losed the game';
+      displayMessage('🫂OOPS you losed the game');
       document.querySelector('.score').textContent = 0;
     }
   }
