5b513d7d2d131089a28f6f443c6fce20c8f400b2
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-03-21 18:41:32 +0530

    Added refactoring of code from closeModal And openModal

--- 02-Modal/script.js
+++ 02-Modal/script.js
@@ -3,13 +3,24 @@ const modal = document.querySelector('.modal');
 const btn = document.querySelectorAll('.show-modal');
 const overlay = document.querySelector('.overlay');
 const closemodal = document.querySelector('.close-modal');
+
+//Open Modal
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+//Close Modal
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
 for (let i = 0; i < btn.length; i++) {
   btn[i].addEventListener('click', function (e) {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+    openModal();
   });
 }
 closemodal.addEventListener('click', () => {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  closeModal();
 });
