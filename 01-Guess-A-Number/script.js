978dd0bfb5a9a8b627a92b164d5ef3626cda76a6
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-03-21 14:50:58 +0530

    Added the function to generate the randam Value

--- 01-Guess-A-Number/script.js
+++ 01-Guess-A-Number/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+let guessNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(guessNumber);
+let Score = 20;
+let higherscore;
+
+document.querySelector('.number').textContent = guessNumber;
+
+let inputNumber = Number(document.querySelector('.guess').textContent);
+console.log(inputNumber);
+
+document.querySelector('.check').addEventListener('click', function () {
+  //First condition if User didnot enter any Think
+  if (!inputNumber) {
+    document.querySelector('.message').textContent = '🙏Please enter the Value';
+  }
+  console.log('Inside function');
+});
