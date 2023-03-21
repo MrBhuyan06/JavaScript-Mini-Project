5943ed73901797f3d0d76a811b1d82d813e00960
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-03-21 15:22:13 +0530

    implement the check button

--- 01-Guess-A-Number/script.js
+++ 01-Guess-A-Number/script.js
@@ -3,17 +3,38 @@
 let guessNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(guessNumber);
 let Score = 20;
-let higherscore;
-
-document.querySelector('.number').textContent = guessNumber;
-
-let inputNumber = Number(document.querySelector('.guess').textContent);
-console.log(inputNumber);
-
+let higherscore = 0;
 document.querySelector('.check').addEventListener('click', function () {
+  // acess the input value
+  let inputNumber = Number(document.querySelector('.guess').value);
+  console.log(inputNumber);
   //First condition if User didnot enter any Think
   if (!inputNumber) {
     document.querySelector('.message').textContent = '🙏Please enter the Value';
   }
-  console.log('Inside function');
+  //Second check is the guess number is equal to the input number
+  else if (guessNumber === inputNumber) {
+    document.querySelector('.message').textContent =
+      '🏆 You won the match Buddy';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').textContent = guessNumber;
+    document.querySelector('.number').style.width = '30rem';
+
+    if (Score > higherscore) {
+      higherscore = Score;
+      document.querySelector('.highscore').textContent = higherscore;
+    }
+  } else if (guessNumber !== inputNumber) {
+    if (Score > 1) {
+      inputNumber > guessNumber
+        ? (document.querySelector('.message').textContent = '📈 Too High')
+        : (document.querySelector('.message').textContent = '📈 Too Low');
+      Score--;
+      document.querySelector('.score').textContent = Score;
+    } else {
+      document.querySelector('.message').textContent =
+        '🫂OOPS you losed the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
 });
