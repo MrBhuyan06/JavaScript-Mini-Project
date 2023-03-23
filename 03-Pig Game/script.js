a363313b8eb13201a42f1945df4b448ec6e1764e
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-03-23 12:06:40 +0530

    implement the add score to current Score

--- 03-Pig Game/script.js
+++ 03-Pig Game/script.js
@@ -3,6 +3,9 @@
 //Selecting Elements
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -17,14 +20,26 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   //1.Generation a random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
 
   //2.Display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
 
   //3.check for rolled 1: if true , Switch to next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    //TODO CHANGE LATER
+    current0El.textContent = currentScore;
+  }
+  //switch to next Player
+  else {
+  }
 });
