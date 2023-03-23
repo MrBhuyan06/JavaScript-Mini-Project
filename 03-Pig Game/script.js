b5cf319d3a556313c02be2a3057beebd762aff7d
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-03-23 11:58:48 +0530

    Made the rool dice and load dofferent pic

--- 03-Pig Game/script.js
+++ 03-Pig Game/script.js
@@ -3,7 +3,28 @@
 //Selecting Elements
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+console.log(diceEl);
 console.log(score0El.textContent);
 console.log(score1El.textContent);
+
+//Staring conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //1.Generation a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //2.Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //3.check for rolled 1: if true , Switch to next player
+});
