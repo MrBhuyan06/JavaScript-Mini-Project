94e6fcb81edf37acf78b8d92a0251540ffd2bfd8
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-03-23 12:56:16 +0530

    Done With the how to made the btn desable

--- 03-Pig Game/script.js
+++ 03-Pig Game/script.js
@@ -34,50 +34,59 @@ diceEl.classList.add('hidden');
 const score = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 
 //Rolling dice functionality
+
 btnRoll.addEventListener('click', function () {
-  //1.Generation a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+  if (playing) {
+    //1.Generation a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
 
-  //2.Display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    //2.Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  //3.check for rolled 1: if true , Switch to next player
-  if (dice !== 1) {
-    // Add dice to current score
-    currentScore += dice;
-    //TODO CHANGE LATER dynamic add score to the active player
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-    // current0El.textContent = currentScore;
-  } else {
-    //switch to next Player
-    switchPlayer();
+    //3.check for rolled 1: if true , Switch to next player
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      //TODO CHANGE LATER dynamic add score to the active player
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+      // current0El.textContent = currentScore;
+    } else {
+      //switch to next Player
+      switchPlayer();
+    }
   }
 });
 
 btnHold.addEventListener('click', function () {
-  //1.Add Current score to active player's score
-  score[activePlayer] += currentScore;
-  //   score[1] = score[1] + currentScore;
-  document.getElementById(`score--${activePlayer}`).textContent =
-    score[activePlayer];
+  if (playing) {
+    //1.Add Current score to active player's score
+    score[activePlayer] += currentScore;
+    //   score[1] = score[1] + currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      score[activePlayer];
 
-  //2.check if player's score i s>= 100
-  if (score[activePlayer] >= 20) {
-    // Finish the game
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add('player--winner');
+    //2.check if player's score i s>= 100
+    if (score[activePlayer] >= 20) {
+      // Finish the game
+      playing = false;
+      //hidde the dic
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
 
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.remove('player--active');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    }
+
+    //switch the next player
+    switchPlayer();
   }
-
-  //switch the next player
-  switchPlayer();
 });
